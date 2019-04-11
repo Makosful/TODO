d0ea5f0c641a49f79b91110679ed643d22b2e599
Axl <makosful@gmail.com>
2019-04-11 10:05:57 +0200

    Initialized the InMemory storage in the DAL Facade

--- app/src/main/java/com/github/makosful/todo/dal/DataLayerFacade.java
+++ app/src/main/java/com/github/makosful/todo/dal/DataLayerFacade.java
@@ -1,8 +1,17 @@
 package com.github.makosful.todo.dal;
 
+import com.github.makosful.todo.be.Todo;
+import com.github.makosful.todo.dal.storage.IStorage;
+import com.github.makosful.todo.dal.storage.TodoInMemory;
+
 /**
  * This class serve as the Data Access Facade and will delegate data calls to their respective
  * implementations within the DAL package.
  */
 public class DataLayerFacade implements IDAL {
+    private IStorage<Todo> todoStorage;
+
+    public DataLayerFacade() {
+        this.todoStorage = new TodoInMemory();
+    }
 }
