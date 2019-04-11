be3286311e37c9e5991288b20a337fd8028383d3
Makos <32297912+Makosful@users.noreply.github.com>
2019-04-11 17:50:31 +0200

    Added a new MainActivity to act as a launcher

---
+++ app/src/main/java/com/github/makosful/todo/gui/controller/MainActivity.java
@@ -0,0 +1,26 @@
+package com.github.makosful.todo.gui.controller;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.makosful.todo.R;
+import com.github.makosful.todo.gui.controller.TodoList;
+import com.github.makosful.todo.gui.model.MainModel;
+
+/**
+ * Launcher Activity.
+ * This Activity will serve as a launcher and perform startup-only actions
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new MainModel(this).seedStorage();
+
+        startActivity(new Intent(this, TodoList.class));
+    }
+}
