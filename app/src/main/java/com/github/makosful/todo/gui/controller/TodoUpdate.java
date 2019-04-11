11ef6c7f65884ef181ee4becd02f309a56c181f4
Makos <32297912+Makosful@users.noreply.github.com>
2019-04-11 17:22:05 +0200

    Added other CRUD Activities for Todo Business Activity

---
+++ app/src/main/java/com/github/makosful/todo/gui/controller/TodoUpdate.java
@@ -0,0 +1,15 @@
+package com.github.makosful.todo.gui.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.makosful.todo.R;
+
+public class TodoUpdate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_update);
+    }
+}
