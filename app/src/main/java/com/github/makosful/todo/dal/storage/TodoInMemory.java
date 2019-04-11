2635af7634787a552f8d2a7e60f7268121ee5f1d
Makos <32297912+Makosful@users.noreply.github.com>
2019-04-11 17:51:01 +0200

    Implemented TodoInMemory

--- app/src/main/java/com/github/makosful/todo/dal/storage/TodoInMemory.java
+++ app/src/main/java/com/github/makosful/todo/dal/storage/TodoInMemory.java
@@ -2,34 +2,58 @@ package com.github.makosful.todo.dal.storage;
 
 import com.github.makosful.todo.be.Todo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * This class is a Mock class meant to simulate a Database
  */
 public class TodoInMemory implements IStorage<Todo> {
+    private static List<Todo> todoList;
+
+    public TodoInMemory() {
+        if (todoList == null)
+            todoList = new ArrayList<>();
+    }
+
     @Override
     public boolean create(Todo item) {
-        return false;
+        return todoList.add(item);
     }
 
     @Override
     public Todo read(int id) {
+        for (Todo todo: todoList) {
+            if (todo.getId() == id) {
+                return todo;
+            }
+        }
         return null;
     }
 
     @Override
     public List<Todo> readAll() {
-        return null;
+        return todoList;
     }
 
     @Override
     public boolean update(Todo item) {
-        return false;
+        int index = -1;
+        for (int i = 0; i < todoList.size(); i++) {
+            if (todoList.get(i).getId() == item.getId()) {
+                index = i;
+            }
+        }
+
+        if (index < 0) return false;
+        else {
+            todoList.set(index, item);
+            return true;
+        }
     }
 
     @Override
     public boolean delete(int id) {
-        return false;
+        return todoList.remove(read(id));
     }
 }
